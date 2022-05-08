87ba845de464892d7800e152cca1c7a6876abfb9
cfirksi <cfirski@protonmail.com>
2022-05-08 17:10:53 +0300

    .

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -4,7 +4,7 @@ app = express();
 var response;
 
 app.get('/', function (req, res) {
-  response = 'This is version 2 of the app.' + '\n';
+  response = 'This is version 3 of the app.' + '\n';
 
   res.send(response);
 });
